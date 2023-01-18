051294e02ccad1bf2bb5709cf1b16517bc2dd2dc
Matias Rodriguez <matiirodriguez08@gmail.com>
2023-01-18 14:50:20 -0300

    proyecto final

---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import {Peliculas,categorias} from '../../mock'
+import Item from '../Item/Item'
+import './ItemListContainer.css'
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+
+const ItemListContainer = () => {
+  const [item,setItem] = useState(Peliculas);
+  const {id} = useParams()
+
+  const FilterCategory = new Promise((resolve,eject)=>{
+    if(id){
+      const newPeliculas = Peliculas.filter((p)=> p.category == id)
+      resolve(newPeliculas)
+    }
+    else{
+      resolve(Peliculas)
+    }
+    
+  })
+
+  useEffect(()=>{
+    FilterCategory.then((response)=>{
+      setItem(response)
+    })
+  },[id])
+  return(
+    <div className='itemlistcontainer'>
+      {
+        item && item.map((producto)=>{
+          return <Item producto={producto} />
+        })
+      }
+    </div>
+  )
+};
+
+export default ItemListContainer;
---
+++ src/mock.js
@@ -0,0 +1,70 @@
+export const categorias = [
+    {
+        name:'Acción y aventura',
+        id:1,
+    },
+    {
+        name:'Familia',
+        id:2,
+
+    },
+    {
+        name:'Terror',
+        id:3,
+    },
+    {
+        name:'Comedia',
+        id:4,
+    },
+    {
+        name:'Ciencia ficción',
+        id:5,
+    },
+]
+
+export const Peliculas = [
+    {
+        id:10,
+        name:'The Avengers',
+        category:1,
+        img:'https://static.wikia.nocookie.net/marvelcinematicuniverse/images/2/2b/The_Avengers_Poster.png/revision/latest?cb=20150610135853&path-prefix=es',
+        
+        
+    },
+    {
+        id:11,
+        name:'Avengers era de Ultron',
+        category:2,
+        img:'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/73121577CB44B82F8EB4580355C4A6BFEB50AEA0BD348456C96D032BDA01CD23/scale?width=506&aspectRatio=2.00&format=jpeg',
+         
+        
+    },
+    {
+        id:12,
+        name:'Avengers Infinity War',
+        category:3,
+        img:'https://static.wikia.nocookie.net/marvelcinematicuniverse/images/9/94/Avengers_Infinity_War_-_P%C3%B3ster_oficial.png/revision/latest?cb=20191029195530&path-prefix=es',
+        
+        
+    },
+    {
+        id:13,
+        name:'Avengers Endgame',
+        category:4,
+        img:'https://www.tematika.com/media/catalog/Ilhsa/Imagenes/667681.jpg',
+       
+    },
+    {
+        id:14,
+        name:'Black Widow',
+        category: 5,
+        img:'https://play-lh.googleusercontent.com/kyp2sHm5j8Wg3ZZoKObeMDylyTzIYJc_WAZJId-EZptCH6nHR-_dHhVrWtUv5wgajI59yLfFJpbYe7u4SVeX',
+        
+    }
+]
+
+export const menus = [{
+    name: 'Inicio',
+    href: '/'
+},
+]
